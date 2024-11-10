f9df0494e9500e92b0d735e13d0c21740b9416a4
Sven Kang <sven.c.kang@gmail.com>
2024-11-10 17:01:04 -0600

    day4 part 2 done

--- 2023/day4/main.go
+++ 2023/day4/main.go
@@ -10,7 +10,14 @@ func main() {
 	bytes, _ := ioutil.ReadFile("d4-2")
 	str := string(bytes)
 	lines := strings.Split(str, "\n")
+	sum1 := part1(lines)
+	sum2 := part2(lines)
 
+	fmt.Printf("answer: %d\n", sum1)
+	fmt.Printf("answer2: %d\n", sum2)
+}
+
+func part1(lines []string) int {
 	sum := 0
 	for _, line := range lines {
 		cols := strings.Split(line, ":")
@@ -44,5 +51,51 @@ func main() {
 			sum += i
 		}
 	}
-	fmt.Printf("answer: %d", sum)
+	return sum
+}
+
+func part2(lines []string) int {
+	sum := 0
+	cardsCount := make([]int, len(lines) - 1)
+	for i, _ := range cardsCount {
+		cardsCount[i] = 1
+	}
+
+	for ci, line := range lines {
+		cols := strings.Split(line, ":")
+		if len(cols) > 1 {
+			numCols := strings.Split(cols[1], "|")
+			wins := strings.Split(numCols[0], " ")
+			winMap := map[string]bool{}
+			for _, w := range wins {
+				winMap[string(w)] = false
+			}
+			rounds := strings.Split(numCols[1], " ")
+			for _, r := range rounds {
+				if string(r) == "" {
+					continue
+				}
+				_, ok := winMap[string(r)]
+				if ok {
+					winMap[string(r)] = true
+				}
+			}
+			winCounts := 0
+			for _, v := range winMap {
+				if v {
+					winCounts++
+				}
+			}
+			for j := 0; j < winCounts; j++ {
+				holdings := cardsCount[ci]
+				cardsCount[ci + j + 1] = cardsCount[ci+ j + 1] + holdings
+			}
+		}
+	}
+
+	for _, v := range cardsCount {
+		sum = sum + v
+	}
+
+	return sum
 }
